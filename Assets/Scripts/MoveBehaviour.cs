a3d65558add4e6e8fa5384f00f300e59b9c82186
hresi <hugo.resina.7e7@itb.cat>
2025-11-07 11:26:11 +0100

    jump logic finnished

--- Assets/Scripts/MoveBehaviour.cs
+++ Assets/Scripts/MoveBehaviour.cs
@@ -10,17 +10,20 @@ public class MoveBehaviour : MonoBehaviour
     private Animator _animator;
     private bool _isJumping = false;
 
+    [SerializeField] private Transform groundCheckPoint; 
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField]
+    private float groundCheckDistance = 0.15f;
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
         _animator = GetComponent<Animator>();
     }
-    //private void FixedUpdate()
-    //{
-    //    RaycastHit hit;
-
-    //}
+    private void Update()
+    {
+        CheckGround();
+    }
     public void MoveCharacter(Vector2 direction)
     {        
         _rb.linearVelocity = new Vector2(direction.x * speed, _rb.linearVelocityY);
@@ -43,14 +46,40 @@ public class MoveBehaviour : MonoBehaviour
         {
             _rb.AddForce(new Vector2(0, speed * 100 * dir));
             _rb.gravityScale *= -1f;
+
             Vector3 scale = transform.localScale;
-            scale.y *= _rb.gravityScale > 0 ? 1 : -1;
+            scale.y = Mathf.Abs(scale.y) * (_rb.gravityScale > 0 ? 1 : -1);
             transform.localScale = scale;
+
             _isJumping = true;
         }
 
         _animator.SetBool("IsJumping", _isJumping);
         //añadir raycast que al colisionar con suelo cambie _isjumping a false
     }
-   
+    private void CheckGround()
+    {
+        if (_rb == null) return;
+
+
+        Vector2 origin = groundCheckPoint.position;
+
+        
+        Vector2 direction = _rb.gravityScale > 0 ? Vector2.down : Vector2.up;
+
+        
+        RaycastHit2D hit = Physics2D.Raycast(origin, direction, groundCheckDistance, groundLayer);
+
+      
+        bool wasJumping = _isJumping;
+        _isJumping = hit.collider == null; 
+
+       
+        if (wasJumping != _isJumping) _animator.SetBool("IsJumping", _isJumping);
+
+        
+        Debug.DrawRay(origin, direction * groundCheckDistance, hit.collider != null ? Color.green : Color.red);
+        
+    }
+
 }
